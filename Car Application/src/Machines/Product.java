5498508126faca3d2072744edd5307cb3f2f21c5
PriyankaM06091994 <59594174+PriyankaM06091994@users.noreply.github.com>
2020-05-18 13:58:25 -0400

    Car Application
    This application allows for the creation and display of a car object with various attributes that best describes the car. It includes various attributes ranging from color, model, serial number, dimensions, etc.

---
+++ Car Application/src/Machines/Product.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Machines;
+
+/**
+ *
+ * @author Priyanka Malpekar
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String availNum;
+    private String description;
+    private String colour;
+    private String model;
+    private String speed;
+    private String wheels;
+    private String doors;
+    private String seats;
+    private String fuel;
+    private String RegNum;
+    private String PlateNum;
+    private String Engine;
+    private String Look;
+    private String Path;
+
+    public String getPath() {
+        return Path;
+    }
+
+    public void setPath(String Path) {
+        this.Path = Path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(String wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getDoors() {
+        return doors;
+    }
+
+    public void setDoors(String doors) {
+        this.doors = doors;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getRegNum() {
+        return RegNum;
+    }
+
+    public void setRegNum(String RegNum) {
+        this.RegNum = RegNum;
+    }
+
+    public String getPlateNum() {
+        return PlateNum;
+    }
+
+    public void setPlateNum(String PlateNum) {
+        this.PlateNum = PlateNum;
+    }
+
+    public String getEngine() {
+        return Engine;
+    }
+
+    public void setEngine(String Engine) {
+        this.Engine = Engine;
+    }
+
+    public String getLook() {
+        return Look;
+    }
+
+    public void setLook(String Look) {
+        this.Look = Look;
+    }
+    
+    
+    
+}
